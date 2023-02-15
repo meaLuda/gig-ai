72d73ca6d78dea6519aa730cb79e76a2fb316213
meaLuda <munyalamea@gmail.com>
2023-02-15 12:51:44 +0300

    local update

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ if os.environ['DEV'] == True:
 else:
     HOST ="localhost"
     config = dotenv_values(".env")
-    openai.api_key = config['OPENAI_API_KEY']
+    openai.api_key = config['OPENAI_API_KEY_TEST']
     
 
 # use your own key here
