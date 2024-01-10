f2c7419b4afe2785f6b5b637b569b5982c262ca1
meaLuda <munyalamea@gmail.com>
2024-01-10 09:31:54 +0300

    small change

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ import uvicorn
 from fastapi import FastAPI
 import openai
 
-
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import datetime
 import redis
 from dotenv import load_dotenv,dotenv_values
+from langchain.llms import HuggingFaceHub
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
 
 load_dotenv()
 
@@ -34,50 +36,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 # docker run -p 6379:6379 -it redis/redis-stack:latest
-
-# Basemodel for the job description
-class Prompt(BaseModel):
-    prompt: str
-    license: str
-    email: str
-
-env_dev = os.environ['DEV']
-# print(env_dev)
-
-# environment set-up must have these in your local and dev path
-if  env_dev == 'True':
-    env_key = os.environ['OPENAI_API_KEY']
-    HOST = "176.58.111.181"
-    openai.api_key = env_key
-
-if env_dev != 'True':
-    HOST ="localhost"
-    config = dotenv_values(".env")
-    openai.api_key = config['OPENAI_API_KEY_TEST']
-    
-
-# use your own key here
-    
-
-# Define the key for storing the count of prompts for each user license
-KEY_TEMPLATE = 'prompt-count-{}-{}'
-
-# prompts to chat with gigai
-GIG_PROMPTS = """
-Given a freelance job description that will start after: Respond accordingly to the following job description:
-Generate a proposal or cover letter that provides a breakdown of how I will solve the tasks outlined in the given job description, 
-outline any possible specific tools or processes that the I can use to complete the job, and provide a breakdown of how 
-I plan to accomplish each task using the specified tools or processes, give me best practices, tools, or processes to complete the job and explain how these will be employed to achieve the best possible outcome.
-Do not talk about my experience or skills. Rather focus on the tasks, tools, and processes that to be used to complete the job. make it solution oriented.
-If the input is not a job description, make sure to only reply with: "Sorry, this does not look like a job description, please try again."
-Respond accordingly to the following job description: \n \n
-"""
-
-# Define the limit of prompts per day per user license
-PROMPT_LIMIT = 15
-# PROMPT_LIMIT = 3
-
-count = 0 # count of times the root has been called
+HUGGINGFACEHUB_API_TOKEN = os.environ["HUGGINGFACEHUB_API_TOKEN"] 
 @app.get("/")
 async def root():
     global count
@@ -90,45 +49,34 @@ def get_current_date():
 
 # route for the prompt
 @app.post("/api/gigai/")
-async def gigai(prompt: Prompt):
-    
-    user_license = prompt.license
-    current_date = get_current_date()
-    key = KEY_TEMPLATE.format(user_license, current_date)
+async def gigai():
+    try:
+        question = f"""
+        Given a freelance job description that will start after: Respond accordingly to the following job description:
+        Generate a proposal or cover letter that provides a breakdown of how I will solve the tasks outlined in the given job description, 
+        outline any possible specific tools or processes that the I can use to complete the job, and provide a breakdown of how 
+        I plan to accomplish each task using the specified tools or processes, give me best practices, tools, or processes to complete the job and explain how these will be employed to achieve the best possible outcome.
+        Do not talk about my experience or skills. Rather focus on the tasks, tools, and processes that to be used to complete the job. make it solution oriented.
+        If the input is not a job description, make sure to only reply with: "Sorry, this does not look like a job description, please try again."
+        """
+        
+        template = """Question: {question}
+            Answer: Let's think step by step.
+        """
+        # See https://huggingface.co/models?pipeline_tag=text-generation&sort=downloads for some other options
+        # Prompt template to the llm
+        repo_id = "google/flan-t5-xxl"
+        prompt =PromptTemplate(template=template, input_variables=["question"])
 
-    # Increment the count of prompts for this user license and date
-    prompt_count = r.incr(key)
-    
-    # print(
-    #     {
-    #         "user_license": user_license,
-    #         "current_date": current_date,
-    #         "key": key,
-    #         "prompt_count": prompt_count,
-    #     }
-    # )
-
-    # If the count exceeds the limit, return an error
-    if prompt_count > PROMPT_LIMIT:
-        return {"error": "Too many prompts today."}
-
-    if prompt.prompt and isinstance(prompt.prompt, str) and len(prompt.prompt.strip()) > 30:
-        # remove await if not working.
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt= GIG_PROMPTS + " \n \n "+prompt.prompt,
-            temperature=0.7,
-            max_tokens=1000,
-            top_p=1,
-            frequency_penalty=1,
-            presence_penalty=1
+        llm = HuggingFaceHub(
+            repo_id=repo_id,
+            model_kwargs={"tempreture":0.5,"max_lenght":1000}
         )
-
-        # Your code to handle the prompt goes here
-        # ...
-        return {"message": response.choices[0].text}
-    else:
-            return {"message": "Sorry this does not look like a job description, try again"}
+        llm_chain =  LLMChain(prompt=prompt,llm=llm)
+        a = llm_chain.run(question)
+        return {"message": a}
+    except Exception as e:
+        return {"message": "Sorry this does not look like a job description, try again"}
 
 # change the host here to local host.
 if __name__ == "__main__":
